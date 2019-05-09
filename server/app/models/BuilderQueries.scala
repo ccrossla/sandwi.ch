b8811a1992da692ca09fdc0e639b656ef53407c2
Quinn Butterfield <qbutterf@trinity.edu>
2019-05-09 12:57:11 -0500

    git shit

--- server/app/models/BuilderQueries.scala
+++ server/app/models/BuilderQueries.scala
@@ -7,19 +7,14 @@ import scala.concurrent.Future
 import Tables._
 import scala.concurrent.ExecutionContext
 
-
-object BuilderQueries {
-  def validUser(user: String, pass:String, db: Database)(implicit ec: ExecutionContext): Future[Seq[String]] = {
-    
-  }
- def userTasks(db: Database, user: String)(implicit ec: ExecutionContext): Future[Seq[String]] = {
-    db.run {
-      (for {
-        c <- Customer
-        if c.name === user
-        t <- Tasks
-        if t.cusid === c.id
-      } yield t.task).result
-    }
-  }
-}
+//
+//object BuilderQueries {
+//  def addIngredient(db: Database, sid: Int, iid: Int)(implicit ec:ExecutionContext):Future[Boolean] = {
+//    db.run {
+//     Sandwichingredient += SandwichingredientRow(sid, iid)
+//    }
+//  }
+//  
+//  
+// 
+//}
