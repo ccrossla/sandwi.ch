9d8c437c5b0ad4d6266641128af59fe2085697ad
Quinn Butterfield <qbutterf@trinity.edu>
2019-05-10 00:24:11 -0500

    git shit

--- server/app/controllers/SandwichMaker.scala
+++ server/app/controllers/SandwichMaker.scala
@@ -21,57 +21,70 @@ import scala.concurrent.ExecutionContext
 
 import slick.jdbc.MySQLProfile.api._ // This line determines what type of database you are connecting to.
 
-
 @Singleton
 class SandwichMaker @Inject() (protected val dbConfigProvider: DatabaseConfigProvider, cc: MessagesControllerComponents)(implicit ec: ExecutionContext)
   extends MessagesAbstractController(cc) with HasDatabaseConfigProvider[JdbcProfile] {
-  
-  def build = Action.async { implicit request => 
+
+  def build = Action.async { implicit request =>
     val u = request.session("uid").toInt
-    models.BuilderQueries.addSandwich(db, u)
-    
-    models.BuilderQueries.getAllIngredients(db).flatMap(f => {   
-    models.BuilderQueries.getSid(db, u).map( x =>   
-    Ok(views.html.builder(u, f, x.last._1, x.last._2)))}) 
- 
+    models.BuilderQueries.addSandwich(db, u) //future
+
+    models.BuilderQueries.getAllIngredients(db).flatMap(f => { //then future this is bad
+      models.BuilderQueries.getSid(db, u).map(x =>
+        Ok(views.html.builder(u, f, x.last._1, x.last._2)))
+    })
+
   }
-  def changeName = Action.async{ implicit request =>
-    //sid and sname
+  def changeName = Action.async { implicit request =>
+  
     val postBody = request.body.asFormUrlEncoded
     postBody.map { args =>
       val sid = args("sid").head.toInt
       val sname = args("sname").head
       BuilderQueries.updateName(db, sid, sname).flatMap(f => {
-          models.BuilderQueries.getAllIngredients(db).map(q => Ok(views.html.builder(request.session("uid").toInt, q, sid, sname)))
-        })
+        models.BuilderQueries.getAllIngredients(db).map(q => Ok(views.html.builder(request.session("uid").toInt, q, sid, sname)))
+      })
     }.getOrElse(Future(Redirect("rebuild", 200)))
-    
+
   }
-  
+
   def rebuild = Action.async { implicit request =>
     val u = request.session("uid").toInt
-    models.BuilderQueries.getAllIngredients(db).flatMap(f => {   
-    models.BuilderQueries.getSid(db, u).map( x =>   
-    Ok(views.html.builder(u, f, x.last._1, x.last._2)))}) 
+    models.BuilderQueries.getAllIngredients(db).flatMap(f => {
+      models.BuilderQueries.getSid(db, u).map(x =>
+        Ok(views.html.builder(u, f, x.last._1, x.last._2)))
+    })
   }
-  /*
-  def createSandwich(uid: Int) = Action.async { implicit request =>
-    BuilderQueries.addSandwich(db, uid)
-  }*/
+  
+  
+  
   
   def addIngredient = Action.async { implicit request =>
     val postBody = request.body.asFormUrlEncoded
     postBody.map { args =>
-        val sid = args("sid").head.toInt
-        val iid = args("iid").head.toInt
-        val sname = args("sname").head
-        BuilderQueries.addIngredient(db, sid, iid).flatMap(f => {
-          models.BuilderQueries.getAllIngredients(db).map(q => Ok(views.html.builder(request.session("uid").toInt, q, sid, sname)))
-        })
-     }.getOrElse(Future(Redirect("rebuild", 200)))
+      val sid = args("sid").head.toInt
+      val iid = args("iid").head.toInt
+      val sname = args("sname").head
+      val success = BuilderQueries.addIngredient(db, sid, iid)
+      success.flatMap { q =>
+        if (q > 0) {
+          val allgreeds = models.BuilderQueries.getAllIngredients(db)
+          allgreeds.map { ingred =>
+            if (ingred != None) {
+              Ok(views.html.builder(request.session("uid").toInt, ingred, sid, sname))
+            } else {
+              Redirect(routes.SandwichMaker.rebuild)
+            }
+          }
+
+        } else {
+          Future(Redirect(routes.SandwichMaker.rebuild))
+        }
+      }
+
+    }.getOrElse(Future.successful(Redirect(routes.SandwichMaker.rebuild)))
+
   }
-   
+} 
   
-  
-  
-}
+ 
--- server/app/models/BuilderQueries.scala
+++ server/app/models/BuilderQueries.scala
@@ -7,45 +7,54 @@ import scala.concurrent.Future
 import Tables._
 import scala.concurrent.ExecutionContext
 
-
 object BuilderQueries {
-  def addSandwich(db: Database, uid:Int)(implicit ec:ExecutionContext):Future[Int] = {
+  def addSandwich(db: Database, uid: Int)(implicit ec: ExecutionContext): Future[Int] = {//
+    /*val x = db.run {
+       (for (u <- User 
+        if u.id === uid 
+        ) yield u.username).result
+    }
+    x.map(f=> {*/
     db.run {
-      Sandwiches += SandwichesRow(0, uid, (uid+ "'s sandwich"))  
+      Sandwiches += SandwichesRow(0, uid, (uid + "'s sandwich"))//uname
     }
   }
-  def getSid(db: Database, uid:Int)(implicit ec:ExecutionContext):Future[Seq[(Int, String)]] = {
-    db.run{
-      (for(s <- Sandwiches if s.uid===uid) yield (s.id, s.name)).result
-    }
-  }
-  def updateName(db: Database, sid:Int, sname:String)(implicit ec:ExecutionContext):Future[Int] = {
-    db.run{
-      Sandwiches.filter(_.id === sid)
-       .map(s => s.name)
-       .update(sname)
-    }
-  }
-  
-  def addIngredient(db: Database, sid: Int, iid: Int)(implicit ec:ExecutionContext):Future[Int] = {
+  def getSid(db: Database, uid: Int)(implicit ec: ExecutionContext): Future[Seq[(Int, String)]] = {
     db.run {
-     Sandwichingredient += SandwichingredientRow(sid, iid)
+      (for (s <- Sandwiches if s.uid === uid) yield (s.id, s.name)).result
     }
   }
-  def getAllIngredients(db: Database)(implicit ec:ExecutionContext): Future[Seq[Int]] = {
-     db.run {
-       (for (i <- Ingredients) yield i.id).result
+  def updateName(db: Database, sid: Int, sname: String)(implicit ec: ExecutionContext): Future[Int] = {
+    db.run {
+      val s = for { s <- Sandwiches if s.id === sid } yield s.name 
+      s.update(sname)
     }
-  
-    
   }
-//  QUERIES TO ADD
-//  def resetSandwich
-     //This will delete the sandwich in the table, remove all ingredients associated with it in
-     //sandwichIngredient, and refresh the page, calling addSandwich again to create a new 
-     //sandwich
+
+  def addIngredient(db: Database, sid: Int, iid: Int)(implicit ec: ExecutionContext): Future[Int] = {  
+    db.run {
+      Sandwichingredient += SandwichingredientRow(sid, iid)
+
+    }  
+  }
+  def getAllIngredients(db: Database)(implicit ec: ExecutionContext): Future[Seq[Int]] = {
+    db.run {
+      (for (i <- Ingredients) yield i.id).result
+    }
+  }
+  
+  def getSandwich(db: Database)(implicit ec: ExecutionContext): Future[Seq[Int]] = {
+    db.run {
+      (for (i <- Sandwichingredient) yield i.iid).result
+    }
+  }
   
   
   
- 
+  //  QUERIES TO ADD
+  //  def resetSandwich
+  //This will delete the sandwich in the table, remove all ingredients associated with it in
+  //sandwichIngredient, and refresh the page, calling addSandwich again to create a new
+  //sandwich
+
 }
